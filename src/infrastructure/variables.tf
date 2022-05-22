dac09cff9b80e44a339b294167a85534a42fb5a8
danielmoreano <daniel.moreano.learn@gmail.com>
2022-05-22 23:07:30 +0200

    uncomment unecessary variables

--- src/infrastructure/variables.tf
+++ src/infrastructure/variables.tf
@@ -5,17 +5,19 @@ variable "project" {
 }
 
 #export TF_VAR_region=[varriable-value]
+# This variable is not longer required to set it, but for the purpose of using a variable in github actions we do just do it.
 variable "region" {
   type = string
   description = "The region where the resources will live"
 }
 
-variable "AWS_ACCESS_KEY" {
-  type = string
-  description = "The region where the resources will live"
-}
+# These 2 variables are no longer required to set them since it will be feched from the env variables
+# variable "AWS_ACCESS_KEY" {
+#   type = string
+#   description = "The access key of your AWS user account"
+# }
 
-variable "AWS_SECRET_KEY" {
-  type = string
-  description = "The region where the resources will live"
-}
+# variable "AWS_SECRET_KEY" {
+#   type = string
+#   description = "The secret key of your AWS user account"
+# }
